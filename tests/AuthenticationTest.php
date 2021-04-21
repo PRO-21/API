b9352ce7c50cffaf4188966da8a4018d19899510
danydacosta <danydc@comac.ch>
2021-04-21 14:35:23 +0200

    extend auth tests

--- tests/AuthenticationTest.php
+++ tests/AuthenticationTest.php
@@ -15,8 +15,41 @@ class AuthenticationTest extends TestCase
             ])->seeStatusCode(200);
     }
 
+    public function testAuthenticateUsingWrongCredentials()
+    {
+        $this->json('POST', '/auth', [
+            'auth_type' => 'credentials',
+            'email' => 'albert.dupontel@office.com',
+            'password' => 'passw0rd'
+            ])->seeStatusCode(401)->seeJsonEquals(array (
+                'status' => 
+                array (
+                  'code' => 401,
+                  'message' => 'Erreur d\'authentification : email ou mot de passe incorrect',
+                  'request' => '/auth',
+                  'api-version' => env('APP_VERSION'),
+                ),
+                'data' => NULL,
+              ));
+    }
+
     public function testAuthenticateUsingToken()
     {
         $this->json('POST', '/auth', [ 'auth_type' => 'token' ], ['Authorization' => 'Bearer '.env('TEST_TOKEN')])->seeStatusCode(200);
+    }
+
+    public function testAuthenticateUsingWrongToken()
+    {
+        $this->json('POST', '/auth', [ 'auth_type' => 'token' ], ['Authorization' => 'Bearer '.env('TEST_TOKEN').'asd'])->seeStatusCode(401)
+        ->seeJsonEquals(array (
+            'status' => 
+            array (
+              'code' => 401,
+              'message' => 'Erreur d\'authentification',
+              'request' => '/auth',
+              'api-version' => env('APP_VERSION'),
+            ),
+            'data' => NULL,
+          ));
     }
 }
