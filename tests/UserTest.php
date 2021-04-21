808756af4fd222d708e664cf9245ec8beb6a1cb9
danydacosta <danydc@comac.ch>
2021-04-21 17:08:07 +0200

    tests add user

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -62,4 +62,81 @@ class UserTest extends TestCase
             'data' => NULL,
           ]);
     }
+
+    public function testCreateUser() {
+        $this->json('POST', "/user", [
+            'prenom' => 'Pierre',
+            'nom' => 'Poruy',
+            'adresse' => 'Chemin du Cassis 12',
+            'npa' => '1001',
+            'email' => 'poruy.pierre@gmail.com',
+            'motDePasse' => 'pass',
+          ])->seeStatusCode(200)->seeJson([ // Ne peut pas tester le retour exact car ne peut pas prédir l'id du nouvel utilisateur
+            'status' => [
+              'code' => 200,
+              'message' => 'OK',
+              'request' => '/user',
+              'api-version' => env('APP_VERSION'),
+            ]
+          ]);
+
+          // Supprimer l'utilisateur précédemment créé
+          DB::table('Personne')->where('email', '=', 'poruy.pierre@gmail.com')->delete();
+    }
+
+    public function testCreateUserMissingParameter() {
+        $this->json('POST', "/user", [
+            'prenom' => 'Pierre',
+            'nom' => 'Poruy',
+            'adresse' => 'Chemin du Cassis 12',
+            'npa' => '1001',
+            'motDePasse' => 'pass',
+          ])->seeStatusCode(400)->seeJsonEquals([
+            'status' => [
+              'code' => 400,
+              'message' => 'Requête invalide : il manque un ou des paramètre(s)',
+              'request' => '/user',
+              'api-version' => env('APP_VERSION'),
+            ],
+            'data' => NULL,
+          ]);
+    }
+
+    public function testCreateUserInvalidEmail() {
+        $this->json('POST', "/user", [
+            'prenom' => 'Pierre',
+            'nom' => 'Poruy',
+            'adresse' => 'Chemin du Cassis 12',
+            'npa' => '1001',
+            'email' => 'poruy.pierre@gmailcom',
+            'motDePasse' => 'pass',
+          ])->seeStatusCode(400)->seeJsonEquals([
+            'status' => [
+              'code' => 400,
+              'message' => 'Requête invalide : email incorrect',
+              'request' => '/user',
+              'api-version' => env('APP_VERSION'),
+            ],
+            'data' => NULL,
+          ]);
+    }
+
+    public function testCreateUserEmailAlreadyUsed() {
+        $this->json('POST', "/user", [
+            'prenom' => 'Pierre',
+            'nom' => 'Poruy',
+            'adresse' => 'Chemin du Cassis 12',
+            'npa' => '1001',
+            'email' => 'albert.dupontel@bluewin.com',
+            'motDePasse' => 'pass',
+          ])->seeStatusCode(400)->seeJsonEquals([
+            'status' => [
+              'code' => 400,
+              'message' => 'Requête invalide : cet email est déjà utilisé',
+              'request' => '/user',
+              'api-version' => env('APP_VERSION'),
+            ],
+            'data' => NULL,
+          ]);
+    }
 }
