084af7dcdf5fd4f90a8c68a5dd0e8fa07b3b0327
danydacosta <danydc@comac.ch>
2021-04-28 17:03:34 +0200

    user tests

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -4,55 +4,93 @@ use Illuminate\Support\Facades\DB;
 
 class UserTest extends TestCase
 {
+    protected $userId;
+    protected $otherUserId;
+    protected $userToken;
+
+    protected function setUp() : void {
+        parent::setUp();
+
+        // Créer l'utilisateur de test
+        $this->userId = DB::table('Personne')->insertGetId([
+            'prenom' => 'Albert',
+            'nom' => 'Dupontel',
+            'adresse' => 'Rue de la Côte 56',
+            'npa' => '2000',
+            'typeCompte' => 'pro',
+            'email' => 'albert.dupontel@bluewin.com',
+            'motDePasse' => '5b722b307fce6c944905d132691d5e4a2214b7fe92b738920eb3fce3a90420a19511c3010a0e7712b054daef5b57bad59ecbd93b3280f210578f547f4aed4d25', // pass
+            'idPersonnePays' => 'CHE'
+        ]);
+
+        $this->otherUserId = DB::table('Personne')->insertGetId([
+            'prenom' => 'Jean',
+            'nom' => 'Laserre',
+            'adresse' => 'Rue de la Baie 18',
+            'npa' => '3005',
+            'email' => 'laserrej@swisscom.ch',
+            'motDePasse' => '5b722b307fce6c944905d132691d5e4a2214b7fe92b738920eb3fce3a90420a19511c3010a0e7712b054daef5b57bad59ecbd93b3280f210578f547f4aed4d25', // pass
+            'idPersonnePays' => 'CHE'
+          ]);
+
+        // Récupérer un token pour l'utilisateur
+        $json = $this->post('/auth', [
+            'auth_type' => 'credentials',
+            'email' => 'albert.dupontel@bluewin.com',
+            'password' => 'pass'
+        ])->response->getContent();
+            
+        $this->userToken = json_decode($json)->data->token;
+    }
+
+    protected function tearDown() : void {
+        // Suppr l'utilisateur de test
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('Personne')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        parent::tearDown();
+    }
+
     public function testGetUser() {
-        $this->json('GET', '/user/1', [], ['Authorization' => 'Bearer '.env('TEST_TOKEN')])->seeStatusCode(200)->seeJsonEquals([
+        $this->json('GET', "/user/$this->userId", [], ['Authorization' => 'Bearer '.$this->userToken])->seeStatusCode(200)->seeJsonEquals([
             'status' => [
               'code' => 200,
               'message' => 'OK',
-              'request' => '/user/1',
+              'request' => "/user/$this->userId",
               'api-version' => env('APP_VERSION'),
             ],
             'data' => [
-              'idPersonne' => 1,
+              'idPersonne' => $this->userId,
               'prenom' => 'Albert',
               'nom' => 'Dupontel',
               'adresse' => 'Rue de la Côte 56',
               'npa' => '2000',
               'typeCompte' => 'pro',
               'email' => 'albert.dupontel@bluewin.com',
+              "idPays" => "CHE",
+              "nomPays" => "Switzerland",
+              "codePays" => "CH"
             ],
             'count' => 1,
           ]);
     }
 
     public function testGetAnotherUser() {
-        // Créer un utilisateur temporaire pour tester
-        $id = DB::table('Personne')->insertGetId([
-            'prenom' => 'Jean',
-            'nom' => 'Laserre',
-            'adresse' => 'Rue de la Baie 18',
-            'npa' => '3005',
-            'email' => 'laserrej@swisscom.ch',
-            'motDePasse' => 'pass',
-          ]);
 
-        $this->json('GET', "/user/$id", [], ['Authorization' => 'Bearer '.env('TEST_TOKEN')])->seeStatusCode(403)->seeJsonEquals([
+        $this->json('GET', "/user/$this->otherUserId", [], ['Authorization' => 'Bearer '.$this->userToken])->seeStatusCode(403)->seeJsonEquals([
             'status' => [
               'code' => 403,
               'message' => 'Accès interdit',
-              'request' => "/user/$id",
+              'request' => "/user/$this->otherUserId",
               'api-version' => env('APP_VERSION'),
             ],
             'data' => NULL,
           ]);
-
-          // Supprimer l'utliisateur temporaire
-          DB::table('Personne')->where('idPersonne', '=', $id)->delete();
     }
 
     public function testGetNonExistingUser() {
         $nonExistingId = 9999;
-        $this->json('GET', "/user/$nonExistingId", [], ['Authorization' => 'Bearer '.env('TEST_TOKEN')])->seeStatusCode(404)->seeJsonEquals([
+        $this->json('GET', "/user/$nonExistingId", [], ['Authorization' => 'Bearer '.$this->userToken])->seeStatusCode(404)->seeJsonEquals([
             'status' => [
               'code' => 404,
               'message' => 'Aucune information trouvée',
@@ -71,6 +109,7 @@ class UserTest extends TestCase
             'npa' => '1001',
             'email' => 'poruy.pierre@gmail.com',
             'motDePasse' => 'pass',
+            "idPersonnePays" => "CHE"
           ])->seeStatusCode(200)->seeJson([ // Ne peut pas tester le retour exact car ne peut pas prédir l'id du nouvel utilisateur
             'status' => [
               'code' => 200,
@@ -79,12 +118,9 @@ class UserTest extends TestCase
               'api-version' => env('APP_VERSION'),
             ]
           ]);
-
-          // Supprimer l'utilisateur précédemment créé
-          DB::table('Personne')->where('email', '=', 'poruy.pierre@gmail.com')->delete();
     }
 
-    public function testCreateUserMissingParameter() {
+    public function testCreateUserUsingMissingParameter() {
         $this->json('POST', "/user", [
             'prenom' => 'Pierre',
             'nom' => 'Poruy',
@@ -102,7 +138,7 @@ class UserTest extends TestCase
           ]);
     }
 
-    public function testCreateUserInvalidEmail() {
+    public function testCreateUserUsingInvalidEmail() {
         $this->json('POST', "/user", [
             'prenom' => 'Pierre',
             'nom' => 'Poruy',
@@ -110,6 +146,7 @@ class UserTest extends TestCase
             'npa' => '1001',
             'email' => 'poruy.pierre@gmailcom',
             'motDePasse' => 'pass',
+            "idPersonnePays" => "CHE"
           ])->seeStatusCode(400)->seeJsonEquals([
             'status' => [
               'code' => 400,
@@ -121,7 +158,7 @@ class UserTest extends TestCase
           ]);
     }
 
-    public function testCreateUserEmailAlreadyUsed() {
+    public function testCreateUserUsingAlreadyUsedEmail() {
         $this->json('POST', "/user", [
             'prenom' => 'Pierre',
             'nom' => 'Poruy',
@@ -129,6 +166,7 @@ class UserTest extends TestCase
             'npa' => '1001',
             'email' => 'albert.dupontel@bluewin.com',
             'motDePasse' => 'pass',
+            "idPersonnePays" => "CHE"
           ])->seeStatusCode(400)->seeJsonEquals([
             'status' => [
               'code' => 400,
@@ -141,6 +179,74 @@ class UserTest extends TestCase
     }
 
     public function testEditUser() {
-
+        $this->json('PATCH', "/user", [
+            'idPersonne' => $this->userId,
+            'adresse' => "Rue de la Côte 18"
+        ], ['Authorization' => 'Bearer '.$this->userToken])->seeStatusCode(200)->seeJsonEquals([
+            'status' => [
+              'code' => 200,
+              'message' => 'OK',
+              'request' => "/user",
+              'api-version' => env('APP_VERSION'),
+            ],
+            'data' => [
+                [
+                    'idPersonne' => $this->userId,
+                    'prenom' => 'Albert',
+                    'nom' => 'Dupontel',
+                    'adresse' => 'Rue de la Côte 18',
+                    'npa' => '2000',
+                    'typeCompte' => 'pro',
+                    'email' => 'albert.dupontel@bluewin.com',
+                    "idPays" => "CHE",
+                    "nomPays" => "Switzerland",
+                    "codePays" => "CH"
+                ]
+            ],
+            'count' => 1,
+          ]);
     }
+
+    public function testEditUserUsingNoParameters() {
+        $this->json('PATCH', "/user", [ 'idPersonne' => $this->userId ], ['Authorization' => 'Bearer '.$this->userToken])->seeStatusCode(400)->seeJsonEquals([
+            'status' => [
+              'code' => 400,
+              'message' => 'Requête invalide',
+              'request' => '/user',
+              'api-version' => env('APP_VERSION'),
+            ],
+            'data' => NULL,
+          ]);;
+    }
+
+    public function testEditUserAnotherUser() {
+        $this->json('PATCH', "/user", [
+            'idPersonne' => $this->otherUserId,
+            'adresse' => "Rue de la Côte 18"
+        ], ['Authorization' => 'Bearer '.$this->userToken])->seeStatusCode(403)->seeJsonEquals([
+            'status' => [
+              'code' => 403,
+              'message' => 'Accès interdit',
+              'request' => "/user",
+              'api-version' => env('APP_VERSION'),
+            ],
+            'data' => NULL,
+          ]);
+    }
+
+    public function testEditUnknownUser() {
+        $this->json('PATCH', "/user", [
+            'idPersonne' => 9999,
+            'adresse' => "Rue de la Côte 18"
+        ], ['Authorization' => 'Bearer '.$this->userToken])->seeStatusCode(404)->seeJsonEquals([
+            'status' => [
+              'code' => 404,
+              'message' => 'Aucune information trouvée',
+              'request' => "/user",
+              'api-version' => env('APP_VERSION'),
+            ],
+            'data' => NULL,
+          ]);
+    }
+
 }
