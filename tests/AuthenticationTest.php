635c34a023fb9705ebf00e114e42903ed13fb95a
danydacosta <danydc@comac.ch>
2021-04-28 16:27:08 +0200

    tests auth

--- tests/AuthenticationTest.php
+++ tests/AuthenticationTest.php
@@ -3,9 +3,34 @@
 use Firebase\JWT\JWT;
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\DB;
 
 class AuthenticationTest extends TestCase
 {
+    protected $id;
+
+    protected function setUp() : void {
+        parent::setUp();
+
+        // Créer l'utilisateur de test
+        $this->id = DB::table('Personne')->insertGetId([
+            'prenom' => 'Albert',
+            'nom' => 'Dupontel',
+            'adresse' => 'Rue de la Côte 56',
+            'npa' => '2000',
+            'typeCompte' => 'pro',
+            'email' => 'albert.dupontel@bluewin.com',
+            'motDePasse' => '5b722b307fce6c944905d132691d5e4a2214b7fe92b738920eb3fce3a90420a19511c3010a0e7712b054daef5b57bad59ecbd93b3280f210578f547f4aed4d25', // pass
+            'idPersonnePays' => 'CHE'
+        ]);
+    }
+
+    protected function tearDown() : void {
+        // Suppr l'utilisateur de test
+        DB::table('Personne')->where('idPersonne', '=', $this->id)->delete();
+        parent::tearDown();
+    }
+
     public function testAuthenticateUsingCredentials()
     {
         $this->json('POST', '/auth', [
